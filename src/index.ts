faa992a64ff8eceb7f1ec9e6c3c90eebc5cf7153
oderechei <aleksandr.derechei#@gmail.com>
2021-04-15 21:36:44 +0300

    Add two more solutions for Two Numbers Sum, add tests

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import { shortestToChar } from './821-shortest-distance-to-a-character';
-import { twoNumberSum } from './categories/arrays/two-nummbers-sum';
+import {
+  twoNumberSumSolution1,
+  twoNumberSumSolution2,
+  twoNumberSumSolution3
+} from './categories/arrays/two-numbers-sum-dif-0';
 
 console.group('shortestToChar');
 console.log(shortestToChar('loveleetcode', 'e'));
 console.groupEnd();
 
 console.group('twoNumberSum');
-console.log(twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10));
+console.log(twoNumberSumSolution1([3, 5, -4, 8, 11, 1, -1, 6], 10));
+console.log(twoNumberSumSolution2([3, 5, -4, 8, 11, 1, -1, 6], 10));
+console.log(twoNumberSumSolution3([3, 5, -4, 8, 11, 1, -1, 6], 10));
 console.groupEnd();
