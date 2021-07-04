549fedaa219950377fba6d779fd6253d2a28d399
oderechei <aleksandr.derechei#@gmail.com>
2021-07-04 15:39:23 +0300

    Rename folder

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { findMaxConsecutiveOnes2 } from './fun-with-arrays/findMaxConsecutiveOnes';
+import { findMaxConsecutiveOnes2 } from './fun-with-arrays/find-max-consecutive-ones';
 
 console.log(
   findMaxConsecutiveOnes2([1,1,0,1])
